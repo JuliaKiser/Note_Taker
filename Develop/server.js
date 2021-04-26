b6f432005617be6a08733192099dafc072499b4d
J_Kiser <jdkiser228@gmail.com>
2021-04-26 16:31:42 -0400

    adding routes folder, finishing server.js file with listener

--- Develop/server.js
+++ Develop/server.js
@@ -25,4 +25,7 @@ app.get("/notes", function(req, res){
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
-
+// Listener
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+  });
