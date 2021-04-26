abee5e2242174ef0871ce99c4964d73976ce5541
J_Kiser <jdkiser228@gmail.com>
2021-04-26 16:35:12 -0400

    adding router functions etc.. to route files

---
+++ Develop/Routes/apiRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+const fs = require('fs')
+
+let notes = require('../db/db.json')
+
+// Retrieve current notes
+router.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+//This will allow you to create a new note
+router.post('/notes', (req, res) => {
+    var newNote = req.body
+    if (notes.length) {
+        newNote.id = notes[notes.length - 1].id + 1
+    } else {
+        newNote.id = 1
+    }
+    notes.push(newNote)
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+        console.log(err)
+        res.sendStatus(200)
+    })
+})
+
+// This will allow you to delete a new note
+router.delete("/notes/:id", (req, res) => {
+    const removeNote = parseInt(req.params.id)
+    const newArr = notes.filter(note => note.id !== removeNote)
+    fs.writeFile('./db/db.json', JSON.stringify(newArr), err => {
+        notes = newArr
+        res.sendStatus(200)
+    })
+})
+
+module.exports = router
